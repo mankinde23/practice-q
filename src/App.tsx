43374ab5ab02cc725d371e54b65717b05ae1ce08
mankinde23 <damilolamakinde493@gmail.com>
2025-01-19 07:46:15 +0100

    implemented the logic of having the authcontext to root of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import RouterComponent from '@/config/router';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {AuthProvider} from '@/context/authContext';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <AuthProvider>
+        <RouterComponent />
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
