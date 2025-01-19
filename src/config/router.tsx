2bb0f50903cc404a265f1dcf216203a64ab50c27
mankinde23 <damilolamakinde493@gmail.com>
2025-01-19 07:44:19 +0100

    fixed routing errors

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./protectedroute";
-import Home from "../pages/home";
-import Login from "../pages/login";
-import SignUp from "../pages/signup";
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import ProtectedRoute from './protectedroute';
+import Home from '@/pages/home';
+import Login from '@/pages/login';
+import SignUp from '@/pages/signup';
 
 const RouterComponent = () => {
   const authRoutes = [
-    { path: "/login", element: <Login /> },
-    { path: "/signup ", element: <SignUp /> },
+    {path: '/login', element: <Login />},
+    {path: '/signup', element: <SignUp />},
   ];
-  const inAppRoutes = [{ path: "/home", element: <Home /> }];
+  const inAppRoutes = [{path: '/home', element: <Home />}];
 
   return (
     <Routes>
-      {authRoutes.map(({ path, element }) => (
+      {authRoutes.map(({path, element}) => (
         <Route key={path} path={path} element={element} />
       ))}
+      <Route path="/" element={<Navigate to="/login" replace />} />
 
       <Route element={<ProtectedRoute />}>
-        {inAppRoutes.map(({ path, element }) => (
+        {inAppRoutes.map(({path, element}) => (
           <Route key={path} path={path} element={element} />
         ))}
       </Route>
