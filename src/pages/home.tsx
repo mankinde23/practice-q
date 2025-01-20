0bd8577a6f1ba6f0c256f6997c48de3eba6a1957
mankinde23 <damilolamakinde493@gmail.com>
2025-01-20 05:37:51 +0100

    initialized home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Home() {
   return (
     <div>
       Home
+      <p className="text-[20px] text-green-900">Home</p>
     </div>
-  )
+  );
 }
