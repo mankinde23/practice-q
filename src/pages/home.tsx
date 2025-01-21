1113fc4b809ec28460661fd86bf767385e44b599
mankinde23 <damilolamakinde493@gmail.com>
2025-01-21 09:43:21 +0100

    had an issue with resetting password fixed it by working on the logout function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,22 @@
+import {useAuth} from '@/hooks/useAuth';
 import React from 'react';
 
 export default function Home() {
+  const {logout} = useAuth();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await logout();
+    } catch (error: any) {
+    } finally {
+    }
+  };
   return (
     <div>
       Home
       <p className="text-[20px] text-green-900">Home</p>
+      <button onClick={handleSubmit}>LOG OUT </button>
     </div>
   );
 }
