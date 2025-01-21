9ea4cf48d27bbdd1908c4decb6fdc88293a1644f
mankinde23 <damilolamakinde493@gmail.com>
2025-01-21 09:41:43 +0100

    setting up the forgot passowrd route

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,13 +1,20 @@
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import ProtectedRoute from './protectedroute';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
 import SignUp from '@/pages/signup';
+import ForgotPassword from '@/pages/forgotpassword';
 
 const RouterComponent = () => {
   const authRoutes = [
     {path: '/login', element: <Login />},
     {path: '/signup', element: <SignUp />},
+    {path: '/forgotpassword', element: <ForgotPassword />},
   ];
   const inAppRoutes = [{path: '/home', element: <Home />}];
 
