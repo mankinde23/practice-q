2087558b42dd01502b47aeb88c803b39cd5d680c
mankinde23 <damilolamakinde493@gmail.com>
2025-01-28 20:59:27 +0100

    shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': './src/utils',
+      '@assets': './src/assets',
+
+      '@config': './src/config',
+      '@context': './src/context',
+      '@layouts': './src/layouts',
+      '@pages': './src/pages',
+      '@types': './src/types',
+      '@mock': './src/mock',
+      '@test': './src/test',
+    },
+  },
+});
